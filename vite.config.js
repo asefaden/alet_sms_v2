7f981345944c527433a02e5753859f55f114e58c
Assefa Demeke <assede1122@gmail.com>
2026-05-29 13:36:26 +0300

    change

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/app.js',
+                'resources/css/navbar.css',
+                'resources/js/navbar.js'
+            ],
             refresh: true,
         }),
     ],
 });
-
